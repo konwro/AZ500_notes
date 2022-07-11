9aee97a481f40a2c05c5348f42149c38a4f14514
konwro <73890368+konwro@users.noreply.github.com>
2022-07-11 20:06:06 +0200

    Update 1_RBAC_CLI.ps1

--- 1_RBAC_CLI.ps1
+++ 1_RBAC_CLI.ps1
@@ -3,37 +3,37 @@
 //CREATE USER
 
 // create password profile object
- $passwordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-  $passwordProfile.Password = "<>"
+$passwordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+$passwordProfile.Password = "<>"
   
- // connect to AAD
- Connect-AzureAD
+// connect to AAD
+Connect-AzureAD
  
- // identify AAD tenant
-  $domainName = ((Get-AzureAdTenantDetail).VerifiedDomains)[0].Name
+// identify AAD tenant
+$domainName = ((Get-AzureAdTenantDetail).VerifiedDomains)[0].Name
 
 //create user account
  New-AzureADUser -DisplayName '<>' -PasswordProfile $passwordProfile -UserPrincipalName "<>" -AccountEnabled $true -MailNickName '<>'
  
- // list Azure users
- Get-AzureADUser
+// list Azure users
+Get-AzureADUser
  
- //CREATE SECURITY GROUP
+//CREATE SECURITY GROUP
  
- // create new security group
-  New-AzureADGroup -DisplayName '<>' -MailEnabled $false -SecurityEnabled $true -MailNickName <>
+// create new security group
+New-AzureADGroup -DisplayName '<>' -MailEnabled $false -SecurityEnabled $true -MailNickName <>
   
-  // list groups in AAD tenant
-   Get-AzureADGroup
-   
-   // obtain reference to the user account
-    $user = Get-AzureADUser -Filter "MailNickName eq '<>'"
+// list groups in AAD tenant
+Get-AzureADGroup
+
+// obtain reference to the user account
+$user = Get-AzureADUser -Filter "MailNickName eq '<>'"
     
-    // add user account to security group
-     Add-AzADGroupMember -MemberUserPrincipalName $user.userPrincipalName -TargetGroupDisplayName "<>" 
+// add user account to security group
+Add-AzADGroupMember -MemberUserPrincipalName $user.userPrincipalName -TargetGroupDisplayName "<>" 
      
-     // verify that group contains user
-      Get-AzADGroupMember -GroupDisplayName "<>"
+// verify that group contains user
+Get-AzADGroupMember -GroupDisplayName "<>"
       
       
     
